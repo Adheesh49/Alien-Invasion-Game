5f9f97ff79252fdfbcdd8acd353c1a0672031589
Adheesh <adheeshpandey49@gmail.com>
2025-01-24 13:42:20 +1100

    Small error fixed.'

--- game_stats.py
+++ game_stats.py
@@ -4,8 +4,8 @@ class GameStats:
     def __init__(self, ai_game):
         """Initialize statistics."""
         self.settings = ai_game.settings
-        self.reset.stats()
+        self.reset_stats()
 
-        def reset_stats(self):
-            """Initialize statistics that can change during the game."""
-            self.ships_left = self.settings.ship_limit
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
