b78c0b8c9108122aff6b003ea2fdf439b5a759c1
Adheesh <adheeshpandey49@gmail.com>
2025-01-24 13:36:29 +1100

    The game now responds to the collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,9 @@
 import sys
+from time import sleep
 
 import pygame
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -19,6 +21,9 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+
+        # Create an instance to store game statisitcs
+        self.stats = GameStats(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -140,7 +145,7 @@ class AlienInvasion:
 
          # Look for alien-ship collisions.
          if pygame.sprite.spritecollideany(self.ship, self.aliens):
-              print("Ship hit!!!")
+              self._ship_hit()
               
     def _check_fleet_edges(self):
          """Respond appropriately if any aliens have reached an edge."""
@@ -154,6 +159,22 @@ class AlienInvasion:
          for alien in self.aliens.sprites():
               alien.rect.y += self.settings.fleet_drop_speed
          self.settings.fleet_direction *= -1
+
+    def _ship_hit(self):
+         """Respond to the ship bring hit by an alien"""
+         # Decrement ships_left.
+         self.stats.ships_left -= 1
+
+         # Get rid of any remaining bullets and aliens.
+         self.bullets.empty()
+         self.aliens.empty()
+
+         # Create the new fleet and center the fleet.
+         self._create_fleet()
+         self.ship.center_ship
+
+         # Pause
+         sleep(0, 5)
 
 if __name__ == '__main__':
     #Make a game instance and run a game.
